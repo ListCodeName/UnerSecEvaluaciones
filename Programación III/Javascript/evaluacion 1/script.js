935c5c32cf5f116bcb543ba9de31a6b488e97b4f
listcodename <angelzero16@hotmail.com>
2024-11-18 19:39:08 -0300

    Prog 3 - Evaluación 1

---
+++ Programación III/Javascript/evaluacion 1/script.js
@@ -0,0 +1,15 @@
+// Elementos de la web
+let botonSuma = document.getElementById("sumar");
+let divAtaque = document.getElementById("ataque-valor");
+
+
+
+// Eventos de la web
+botonSuma.addEventListener("click",()=>{
+    let tempAtaque = Number(divAtaque.innerHTML);
+    let tempPuntos = Number(divPuntos.innerHTML);
+
+
+});
+
+
