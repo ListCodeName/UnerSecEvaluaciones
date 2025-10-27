b6a5029618085f43821a8771404290616c38b4ef
ListCodeName <angelzero16@hotmail.com>
2025-10-27 08:33:36 -0300

    prog2 evaluacion2

---
+++ Programación II/evaluación 2/Producto.java
@@ -0,0 +1,20 @@
+public class Producto{
+
+}
+
+/*
+* El producto contará con los siguientes atributos:
+* - Nombre
+* - Precio
+* - Cantidad
+* - Fecha de vencimiento (día-mes-año)
+*
+* Los métodos de la clase serán:
+* - Constructor para todos los atributos
+* - equals(), para comparar un producto que se le pasa por parámetro con el mismo.
+* Serán iguales siempre que tengan el mismo nombre y mismo precio.
+* - toString(), devolverá la información de todos los atributos en el siguiente formato:
+* "Tomate Perita Lata 200g (5 uni.) - $1300 
+*  Vto: 27/10/2026"
+*  
+*/
---
+++ Programación II/evaluación 2/Ticket.java
@@ -0,0 +1,26 @@
+public class Ticket{
+
+}
+
+/*
+* La clase ticket contará con los siguientes elementos: 
+* - El nombre del local
+* - Fecha de emisión del ticket
+* - Lista de productos (Hasta 10 productos Max.)
+*
+* Los métodos serán:
+* - agregarProducto(), se le envía por parámetro el producto en cuestión
+* - eliminarProducto(), se le envía el producto en cuestión y lo elimina del ticket
+* - calcularTotal(), devuelve el precio total de todos los productos que hay en el ticket
+* - imprimirTicket(), debe imprimir la información por pantalla con el siguiente formato:
+* "Empresa distinguida SRL
+* Fecha Emisión: 26/10/2025
+* Productos:
+* Tomate Perita Lata 200g (5 uni.) - $1300 
+* Vto: 27/10/2026
+*
+* Total: $6000
+* "
+*
+*
+*/
